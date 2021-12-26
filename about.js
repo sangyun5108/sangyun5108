2f99fad055029c8bb52d7150ee413ee2a8605ffe
sangyun5108 <69062776+sangyun5108@users.noreply.github.com>
2021-12-26 19:40:59 +0900

    fix : 반응형 웹 페이지 about me 버튼 버그 수정

--- about.js
+++ about.js
@@ -4,7 +4,16 @@ function onClickButton(){
   document.querySelector('.home__to__about').addEventListener('click',(e)=>{
     const link = e.target.dataset.link;
     const about = document.querySelector(link);
-    about.scrollIntoView({block:'center'});
+    let type = '';
+
+    if(document.body.getBoundingClientRect().width<768){
+      type = 'start';
+    }else{
+      type = 'center';
+    }
+
+    about.scrollIntoView({block:type});
+
   });
 }
 
